99ea46c962573eec9093679838e7fcdbe55a33d6
galstylepk7 <galstylepk7@gmail.com>
2026-05-10 09:42:09 +0300

    Update 2026-05-10 09:42

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.firebaseConfig = {
+  apiKey: "AIzaSyCgwXMxx_2MxUWAK7xAAsUYcLR2w6_tO4s",
+  authDomain: "athens-archive-web.firebaseapp.com",
+  projectId: "athens-archive-web",
+  storageBucket: "athens-archive-web.firebasestorage.app",
+  messagingSenderId: "901526170286",
+  appId: "1:901526170286:web:595cccb1079695e57bdb3f"
+};
